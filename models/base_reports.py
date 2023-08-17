6b4714fe78f3b1efdb7ce8a40aa20eee5983fe23
balch027 <balch027@gp2.cla.umn.edu>
2023-08-17 11:35:37 -0500

    New pyproject working

--- models/base_reports.py
+++ models/base_reports.py
@@ -1,4 +1,13 @@
+import xlsxwriter as xl
+import polars as pl
 def metadata_report(metadata):
-    for table in metadata.tables.values():
-        print(f"{table}")
-        print(f"    {table.c}")
+    code_df = [pl.DataFrame({"Table":["Columns"]})]
+    dfs = code_df + [pl.DataFrame({table.name:[col.name for col in table.c]}) for table in metadata.tables.values()]
+    df = pl.concat(dfs, how = "horizontal")
+    build_xl(df, "metadata_report")
+
+def build_xl(table, report_name, file_path=None):
+    if file_path is None:
+        file_path = f"{report_name}.xlsx"
+    with xl.Workbook(file_path) as wb:
+        table.write_excel(workbook = wb)
